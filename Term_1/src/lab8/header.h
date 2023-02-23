076924e6a0d4b033ad899bf1e317d8cc62e2ea1f
Kirill <kirillbond2017@gmail.com>
2023-02-24 02:19:55 +0300

    PROGRAMM_RESTORE

--- Term_1/src/lab8/header.h
+++
@@ -1,8 +0,0 @@
-#include "functions.c"
-void free_mas(char **mas, int r);
-void str_mas_init(char**str_mas,char**argv,int argc);
-void delete_1string(int argc,char** str_mas);
-int check_argc(int argc);
-void copy_array(char **str1, char **str2, int str_num);
-int get_length(char **s, int num);
-void qi_sort(char **mas, int size);
